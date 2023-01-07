44052b66171f4708a7e0cb14689eb5f32194643e
Maria Davila <mariajdab@gmail.com>
2023-01-06 22:46:37 -0400

    main: adding comments

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"math/rand"
 	"time"
@@ -41,10 +42,12 @@ func main() {
 	s.mainLoop()
 }
 
+// mainLoop starts and maintains the simulator until the conditional if not valid
 func (s Simulator) mainLoop() {
-
+	// each alien hasn't moved at least 10k times or all the aliens haven't been destroyed,
+	// also it's considered that the citiesMap could be a nil map, so the condition is added to prevent an error
 	for aliensUnder10kOrAllNil(s.aliens) && s.citiesMap != nil {
-		log.Println(s.aliens, s.cityLedger, s.citiesMap, "este")
+		log.Println(s.aliens, s.cityLedger, s.citiesMap)
 
 		s.updateMap()
 		s.updateAliensInMap()
@@ -53,16 +56,19 @@ func (s Simulator) mainLoop() {
 	}
 }
 
+// updateMap updates the map removing the city from the map if two aliens were in the city, also the two aliens be destroyed
 func (s Simulator) updateMap() {
 	aliensPerCity := make(map[string][]int)
 
 	for index, alien := range s.aliens {
 		if alien != nil {
+			log.Printf("alient: %v index: %d", alien, index)
 			aliensPerCity[alien.Position] = append(aliensPerCity[alien.Position], index)
 		}
 	}
 
 	for city, aliensIndexes := range aliensPerCity {
+		fmt.Println(aliensIndexes)
 		if len(aliensIndexes) == 2 {
 			log.Printf("%s has been destryed by alien %d and alien %d\n", city, aliensIndexes[0], aliensIndexes[1])
 			delete(s.citiesMap, city)
@@ -86,6 +92,7 @@ func (s Simulator) updateMap() {
 	}
 }
 
+// aliensUnder10kOrAllNi in charges of update the alien position for each iteration
 func (s Simulator) updateAliensInMap() {
 	for _, alien := range s.aliens {
 		if alien != nil {
@@ -95,6 +102,7 @@ func (s Simulator) updateAliensInMap() {
 	}
 }
 
+// aliensUnder10kOrAllNi will return false if all the aliens are nil or all the aliens has reached 10k steps
 func aliensUnder10kOrAllNil(aliens []*Alien) bool {
 	for _, alien := range aliens {
 		if alien != nil && alien.StepsCount < maxStepCount {
