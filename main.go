de9d0286dfdfa7a0c64486e502cb0d4306f43673
Maria Davila <mariajdab@gmail.com>
2023-01-07 21:12:31 -0400

    main: comments and small improvements

--- main.go
+++ main.go
@@ -12,14 +12,24 @@ import (
 	"github.com/mariajdab/alien-invasion/simulator"
 )
 
+const worldMap = "cities_list.txt"
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	citiesMap, citiesLedger := file.OpenAndReadCitiesFile("cities_list.txt")
+
+	// the file name is an assumption
+	citiesMap, citiesLedger, err := file.OpenAndReadCitiesFile(worldMap)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// number of aliens through a command-line argument
 	alienNumber, err := strconv.Atoi(os.Args[1])
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	// fill the data needed for each alien with stepCount 0
 	aliens := make([]*alien.Alien, alienNumber)
 	for i := 0; i < alienNumber; i++ {
 		aliens[i] = &alien.Alien{
